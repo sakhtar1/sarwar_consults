f7f8fada037325beacc0c7f396790098d4d49524
Sharmina Akhtar <sakht318@gmail.com>
2018-02-10 18:28:47 +0000

    nomethoderror fix

--- lib/sarwar_consults/cli.rb
+++ lib/sarwar_consults/cli.rb
@@ -10,7 +10,7 @@ class SarwarConsults::CLI
   def introduction
     puts "Welcome to Sarwar Consults!"
     puts "Look at the following services:"
-    list_services  
+    list_services
     puts "To exit at anytime, type exit."
     puts "To sign up at anytime, type sign up."
     puts "What type of services are you looking for today:"
@@ -39,12 +39,12 @@ class SarwarConsults::CLI
       when "sign up"
           puts "Click on the link below to sign up!"
           puts SarwarConsults::Service.scrape_signup
-          
+
       when "exit"
           puts "Until next time!"
       break
-      else
-          puts "I'm sorry, I didn't get that. Can you type that again?"
+      #else
+      #    puts "I'm sorry, I didn't get that. Can you type that again?"
       end
     end
   end
--- lib/sarwar_consults/service.rb
+++ lib/sarwar_consults/service.rb
@@ -6,23 +6,23 @@ class SarwarConsults::Service
       @service = []
     end
     
-    def get_service
+    def self.get_service
       Nokogiri::HTML(open('http://sarwarconsults.com/services/'))
     end
 
-    def scrape_signup
+    def self.scrape_signup
       get_service.search("#menu-item-1663 a").map{|sign| sign['href']}.join("")
     end
 
-    def scrape_services
+    def self.scrape_services
       @service = get_service.search("p a").collect{|item| item.text}
     end
 
-    def scrape_url
+    def self.scrape_url
       @url = get_service.search("p a").collect{|link| link['href']}
     end
 
-    def scrape_content(url)
+    def self.scrape_content(url)
       doc = Nokogiri::HTML(open(url))
       doc.search("p").text.strip
     end
