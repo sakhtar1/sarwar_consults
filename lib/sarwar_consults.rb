008922d8bf2e07c0106040951f6369c3cb34e019
Sharmina Akhtar <sakht318@gmail.com>
2018-02-27 11:44:42 +0000

    bin update

--- lib/sarwar_consults.rb
+++ lib/sarwar_consults.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'pry'
 require 'open-uri'
-require_relative "sarwar_consults/version"
-require_relative './sarwar_consults/cli'
-require_relative './sarwar_consults/service'
+
+require_relative 'sarwar_consults/version'
+require_relative 'sarwar_consults/cli'
+require_relative 'sarwar_consults/service'
